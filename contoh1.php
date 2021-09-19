f8f69626ca253f119a3147f87199e8c0daa72b02
lathifaniafwaz <lathifaniafwaz123@gmail.com>
2021-09-19 17:42:41 +0700

    menambahkan index.html,about.html,contact.html

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Lathifani Afwaz
+        Saya tinggal di daerah Kelapa Gading
+        Hobi saya adalah Fotografi";
+    }
+}
