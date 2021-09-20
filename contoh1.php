eb78b4c83bc3778107e52fba57c6f885154bf9d4
lathifaniafwaz <lathifaniafwaz123@gmail.com>
2021-09-20 16:50:29 +0700

    penambahan isi contoh1.php

--- contoh1.php
+++ contoh1.php
@@ -1,11 +1,11 @@
 <?php
-class Contoh1 extends CI_Controller
+class contoh1 extends CI_Controller
 {
-    public function index()
-    {
-        echo "<h1>Perkenalkan</h1>";
-        echo"Nama saya Lathifani Afwaz
-        Saya tinggal di daerah Kelapa Gading
-        Hobi saya adalah Fotografi";
-    }
+	public function index()
+	{
+		echo "<h1><i>Perkenalan</i></h1>";
+		echo "<br>Nama Saya Lathifani Afwaz</br> 
+			<br>Saya tinggal di daerah Kelapa Gading</br>
+			<br>saya memiliki hobi memotret gambar serta membuat video</br>";
+	}
 }
